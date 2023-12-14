2192fe13c68fdb016e45b0f60f9a33c0431c55dc
DRAKONNN <rafakhis14@gmail.com>
2023-12-14 14:02:01 +0100

    introducing datainfo

---
+++ src/data/datainfo.js
@@ -0,0 +1,18 @@
+export default [{
+  id: 'l01', 
+  name: "Liana",
+  surname: "Unanyan",
+  hair: 'Brown',
+  eyes: 'Brown',
+  height: '1,62 m',
+  bust: '85',
+  hips: '61',
+  hips: '92',
+  shoe: '1,62 m',
+  languages: [
+    'Ruso: bilingüe',
+    'Español: bilingüe',
+    'Inglés: C1',
+    'Catalán: C1'
+  ]
+}];
