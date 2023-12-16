501f20d965ac20b80042e3d5288bca7fcf8a2527
DRAKONNN <rafakhis14@gmail.com>
2023-12-16 17:59:14 +0100

    Introducing images

--- src/App.js
+++ src/App.js
@@ -67,12 +67,7 @@ function App() {
         
         <section className="resume-section" id="contact">
           <Section>
-            <div className={`resume-section-content`}>
-              <h1 className="mb-5">Contacto</h1>
-              <p>Lorem
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
-            </div>
+            
           </Section>
         </section>    
       </div>
--- src/components/stats.js
+++ src/components/stats.js
@@ -14,47 +14,53 @@ function Stats(props) {
   })
   
   return (
-    <div className="resume-section-content">
-      <div className="row pb-5">
-        <div className="container">
-          <table class="table table-light table-striped table-hover border-rounded table-stats">
-            <thead class="table-dark">
-              <tr>
-                <th colspan="2">Características</th>
-              </tr>
-            </thead>
-            <tbody>
-              <tr>
-                <th scope="row">Pelo</th>
-                <td>{stats[0].hair}</td> 
-              </tr>
-              <tr>
-                <th scope="row">Ojos</th>
-                <td>{stats[0].eyes}</td>
-              </tr>
-              <tr>
-                <th scope="row">Altura</th>
-                <td>{stats[0].height}</td>
-              </tr>
-              <tr>
-                <th scope="row">Pecho</th>
-                <td>{stats[0].bust}</td>
-              </tr>
-              <tr>
-                <th scope="row">Cintura</th>
-                <td>{stats[0].waist}</td>
-              </tr>
-              <tr>
-                <th scope="row">Caderas</th>
-                <td>{stats[0].hips}</td>
-              </tr>
-              <tr>
-                <th scope="row">Calzado</th>
-                <td>{stats[0].shoe}</td>
-              </tr>
-            </tbody>
-          </table>
-        </div>
+    <div className="">
+      <div className="row gy-5 pb-4" style={{height: 400}}>  
+          <div className="col-lg-3">
+            <img src="/images/Liana_Winter.JPG" alt="Liana" className="border-rounded img-fluid" />
+          </div>
+          <div className="col-lg-3">
+            <img src="/images/Liana_Winter2.JPG" alt="Liana" className="border-rounded img-fluid" />
+          </div>
+          <div className="col-lg-6">
+            <table class="table table-light table-striped table-hover border-rounded table-stats">
+              <thead class="table-dark">
+                <tr>
+                  <th colspan="2">Características</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row"><i class='fas fa-female'></i> Pelo</th>
+                  <td>{stats[0].hair}</td> 
+                </tr>
+                <tr>
+                  <th scope="row"><i class='far fa-eye'></i> Ojos</th>
+                  <td>{stats[0].eyes}</td>
+                </tr>
+                <tr>
+                  <th scope="row"><i class='fas fa-sort-amount-up'></i> Altura</th>
+                  <td>{stats[0].height}</td>
+                </tr>
+                <tr>
+                  <th scope="row"><i class='fas fa-arrows-alt-h'></i> Pecho</th>
+                  <td>{stats[0].bust}</td>
+                </tr>
+                <tr>
+                  <th scope="row"><i class='fas fa-arrows-alt-h'></i> Cintura</th>
+                  <td>{stats[0].waist}</td>
+                </tr>
+                <tr>
+                  <th scope="row"><i class='fas fa-arrows-alt-h'></i> Caderas</th>
+                  <td>{stats[0].hips}</td>
+                </tr>
+                <tr>
+                  <th scope="row"><i class='fas fa-shoe-prints'></i> Calzado</th>
+                  <td>{stats[0].shoe}</td>
+                </tr>
+              </tbody>
+            </table>
+          </div>
       </div>
     </div>
   );
