f802b9bd0704a393f5a04a9f4dab1f50001d22d2
DRAKONNN <rafakhis14@gmail.com>
2024-01-06 15:03:04 +0100

    Introducing toast empty

--- src/components/contact.js
+++ src/components/contact.js
@@ -13,9 +13,61 @@ function Contact(props) {
   const form = useRef();
   const [showToastSuccess, setShowToastSuccess] = useState(false);
   const [showToastError, setShowToastError] = useState(false);
+  const [showToastEmpty, setShowToastEmpty] = useState(false);
+
+  const [formData, setFormData] = useState({
+    user_name: '',
+    user_email: '',
+    message: ''
+  });
+  const [formError, setFormError] = useState({
+    nameError: '',
+    emailError: '',
+    messageError: ''
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value
+    });
+  };
+
+  const validateForm = () => {
+    let isValid = true;
+    const errors = {
+      nameError: '',
+      emailError: '',
+      messageError: ''
+    };
+
+    if (!formData.user_name.trim()) {
+      errors.nameError = 'Por favor, ingrese su nombre.';
+      isValid = false;
+    }
+
+    if (!formData.user_email.trim()) {
+      errors.emailError = 'Por favor, ingrese su correo electrónico.';
+      isValid = false;
+    }
+
+    if (!formData.message.trim()) {
+      errors.messageError = 'Por favor, ingrese un mensaje.';
+      isValid = false;
+    }
+
+    setFormError(errors);
+    return isValid;
+  };
 
   const sendEmail = (e) => {
+    setShowToastEmpty(true);
     e.preventDefault();
+
+    if (!validateForm()) {
+      return;
+    }
 
     emailjs.sendForm('service_7hz1xkd', 'template_i8i8mt4', form.current, 'FdoqHujR1iZdgJZpM')
       .then((result) => {
@@ -52,19 +104,19 @@ function Contact(props) {
               <label className="form-label">
                 Nombre del remitente
               </label>
-              <input type="text" className="form-control" placeholder="Nombre" name="user_name" />
+              <input type="text" className="form-control" placeholder="Nombre" name="user_name" value={formData.user_name} onChange={handleChange} />
             </div>
             <div className="mb-3">
               <label className="form-label">
                 Email del remitente
               </label>
-              <input type="email" className="form-control" placeholder="name@example.com" name="user_email" />
+              <input type="email" className="form-control" placeholder="name@example.com" name="user_email" value={formData.user_email} onChange={handleChange} />
             </div>
             <div className="mb-3">
               <label className="form-label">
                 Mensaje de texto
               </label>
-              <textarea className="form-control" placeholder="Texto" name="message"></textarea>
+              <textarea className="form-control" placeholder="Texto" name="message" value={formData.message} onChange={handleChange}></textarea>
             </div>
             <button type="submit" value="Send" className="btn btn-primary">
               Enviar
@@ -81,12 +133,20 @@ function Contact(props) {
           <button type="button" className="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close" onClick={() => showToastSuccess(false)}></button>
         </div>
       </div>
-      <div className={`toast align-items-center text-white bg-success border-0 ${showToastError ? 'show' : ''}`} role="alert" aria-live="assertive" aria-atomic="true">
+      <div className={`toast align-items-center text-white bg-danger border-0 ${showToastError ? 'show' : ''}`} role="alert" aria-live="assertive" aria-atomic="true">
         <div className="d-flex">
           <div className="toast-body">
             ¡Error al enviar el correo! Intente enviar otra vez en un rato después de haber refrescado la página.
           </div>
           <button type="button" className="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close" onClick={() => setShowToastError(false)}></button>
+        </div>
+      </div>
+      <div className={`toast align-items-center text-white bg-danger border-0 ${showToastEmpty ? 'show' : ''}`} role="alert" aria-live="assertive" aria-atomic="true">
+        <div className="d-flex">
+          <div className="toast-body">
+            Complete todos los campos, por favor.
+          </div>
+          <button type="button" className="btn-close btn-close-white me-2 m-auto" data-bs-dismiss="toast" aria-label="Close" onClick={() => setShowToastEmpty(false)}></button>
         </div>
       </div>
     </div>
