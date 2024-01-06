b765f5af597c2a1e5080ae04928c05d102b7a87b
DRAKONNN <rafakhis14@gmail.com>
2024-01-06 12:28:18 +0100

    Emailjs developed successfully

--- src/components/contact.js
+++ src/components/contact.js
@@ -4,27 +4,21 @@ import { useState, useCallback, useEffect, useRef } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
-import axios from 'axios';
-
+import emailjs from '@emailjs/browser';
 
 function Contact(props) {
   
-  const [email, setEmail] = useState('');
-  const [message, setMessage] = useState('');
+  const form = useRef();
 
-  const handleEmailChange = (e) => setEmail(e.target.value);
-  const handleMessageChange = (e) => setMessage(e.target.value);
-
-  const handleSubmit = async (e) => {
+  const sendEmail = (e) => {
     e.preventDefault();
 
-    try {
-      await axios.post('http://localhost:3001/send-email', { email, message });
-      alert('Correo enviado correctamente');
-    } catch (error) {
-      console.error('Error al enviar el correo:', error.message);
-      alert('Error al enviar el correo');
-    }
+    emailjs.sendForm('service_7hz1xkd', 'template_i8i8mt4', form.current, 'FdoqHujR1iZdgJZpM')
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
   };
 
   return (
@@ -42,33 +36,27 @@ function Contact(props) {
             <h1 className="mt-4"><a href="tel:+34646155309"><i class='fas fa-phone-square'></i> 646 155 309</a></h1>
             <h2 className="mt-4"><a href="mailto:lianchik.9968@gmail.com"><i class='fas fa-envelope-square'></i>lianchik.9968@gmail.com</a></h2>
             <h1 className="mt-4"><a href="https://www.instagram.com/liaanaa8/"><i class='fab fa-instagram'></i> @liaanaa8</a></h1>
-            <form onSubmit={handleSubmit}>
+
+            <form ref={form} onSubmit={sendEmail}>
               <div className="mb-3">
-                <label htmlFor="exampleFormControlInput1" className="form-label">
-                  Email address
+                <label className="form-label">
+                  Nombre del remitente
                 </label>
-                <input
-                  type="email"
-                  className="form-control"
-                  id="exampleFormControlInput1"
-                  placeholder="name@example.com"
-                  value={email}
-                  onChange={handleEmailChange}
-                />
+                <input type="text" className="form-control" name="user_name" />
               </div>
               <div className="mb-3">
-                <label htmlFor="exampleFormControlTextarea1" className="form-label">
-                  Example textarea
+                <label className="form-label">
+                  Email del remitente
                 </label>
-                <textarea
-                  className="form-control"
-                  id="exampleFormControlTextarea1"
-                  rows="3"
-                  value={message}
-                  onChange={handleMessageChange}
-                ></textarea>
+                <input type="email" className="form-control" placeholder="name@example.com" name="user_email" />
               </div>
-              <button type="submit" className="btn btn-primary">
+              <div className="mb-3">
+                <label className="form-label">
+                  Mensaje de texto
+                </label>
+                <textarea className="form-control" name="message"></textarea>
+              </div>
+              <button type="submit" value="Send" className="btn btn-primary">
                 Enviar
               </button>
             </form>
